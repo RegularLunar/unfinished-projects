4dfb606ecfcda1c663444059b7e0a9db8fc2ec84
Lunar <107058095+RegularLunar@users.noreply.github.com>
2024-11-25 06:43:03 -0800

    Add files via upload

---
+++ Iris - Injector/Iris - Injector/Injector.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Iris___Injector
+{
+    internal class Injector
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr LoadLibraryW(string lpFileName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle, uint dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        public static void InjectDll(int processId, string dllPath)
+        {
+            IntPtr intPtr = Injector.OpenProcess(PROCESS_ALL_ACCESS, false, (uint)processId);
+            if (intPtr == IntPtr.Zero)
+            {
+                int errorCode = Marshal.GetLastWin32Error();
+                Console.Clear();
+                Console.WriteLine("See https://learn.microsoft.com/en-us/windows/win32/debug/system-error-codes--0-499- for more details");
+                throw new Exception($"Failed to open process. Error code: {errorCode}");
+            }
+            uint size = (uint)((dllPath.Length + 1) * Marshal.SizeOf(typeof(char)));
+            IntPtr address = Injector.VirtualAllocEx(intPtr, IntPtr.Zero, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+            if (address == IntPtr.Zero)
+            {
+                int errorCode = Marshal.GetLastWin32Error();
+                Console.Clear();
+                Console.WriteLine("See https://learn.microsoft.com/en-us/windows/win32/debug/system-error-codes--0-499- for more details");
+                throw new Exception($"Failed to allocate memory in the target process. Error code: {errorCode}");
+            }
+            byte[] buffer = Encoding.Unicode.GetBytes(dllPath);
+            int lpNumberOfBytesWritten;
+            if (!Injector.WriteProcessMemory(intPtr, address, buffer, (uint)buffer.Length, out lpNumberOfBytesWritten) || lpNumberOfBytesWritten != buffer.Length)
+            {
+                int errorCode = Marshal.GetLastWin32Error();
+                Console.Clear();
+                Console.WriteLine("See https://learn.microsoft.com/en-us/windows/win32/debug/system-error-codes--0-499- for more details");
+                throw new Exception($"Failed to write memory in the target process. Error code: {errorCode}");
+            }
+            IntPtr moduleHandle = Injector.GetModuleHandle("kernel32.dll");
+            if (moduleHandle == IntPtr.Zero)
+            {
+                int errorCode = Marshal.GetLastWin32Error();
+                Console.Clear();
+                Console.WriteLine("See https://learn.microsoft.com/en-us/windows/win32/debug/system-error-codes--0-499- for more details");
+                throw new Exception($"Failed to get module handle for kernel32.dll. Error code: {errorCode}");
+            }
+            IntPtr loadLibraryAddr = Injector.GetProcAddress(moduleHandle, "LoadLibraryW");
+            if (loadLibraryAddr == IntPtr.Zero)
+            {
+                int errorCode = Marshal.GetLastWin32Error();
+                Console.Clear();
+                Console.WriteLine("See https://learn.microsoft.com/en-us/windows/win32/debug/system-error-codes--0-499- for more details");
+                throw new Exception($"Failed to get LoadLibraryW address. Error code: {errorCode}");
+            }
+            if (Injector.CreateRemoteThread(intPtr, IntPtr.Zero, 0, loadLibraryAddr, address, 0, IntPtr.Zero) == IntPtr.Zero)
+            {
+                int errorCode = Marshal.GetLastWin32Error();
+                Console.Clear();
+                Console.WriteLine("See https://learn.microsoft.com/en-us/windows/win32/debug/system-error-codes--0-499- for more details");
+                throw new Exception($"Failed to create remote thread. Error code: {errorCode}");
+            }
+        }
+
+        const uint PROCESS_CREATE_THREAD = 0x0002;
+        const uint PROCESS_QUERY_INFORMATION = 0x0400;
+        const uint PROCESS_VM_OPERATION = 0x0008;
+        const uint PROCESS_VM_WRITE = 0x0020;
+        const uint PROCESS_VM_READ = 0x0010;
+
+        const uint MEM_COMMIT = 0x1000;
+        const uint MEM_RESERVE = 0x2000;
+        const uint PAGE_READWRITE = 0x04;
+        const uint PROCESS_ALL_ACCESS = 0x001F0FFF;
+    }
+}
---
+++ Iris - Injector/Iris - Injector/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Iris___Injector
+{
+    internal class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            Console.Title = "Iris - Injector, x64 | By RegularLunar";
+            try
+            {
+                Console.WriteLine("Select the executable file.");
+                string processPath = Program.OpenFileBrowser("[ | ] Waiting for .EXE...", "Executable Files (*.exe)|*.exe");
+                if (string.IsNullOrEmpty(processPath))
+                {
+                    Console.Clear();
+                    Console.WriteLine("No executable selected. Exiting.");
+                    Pause();
+                    return;
+                }
+                string processName = Path.GetFileNameWithoutExtension(processPath);
+                Console.Clear();
+                Console.WriteLine("[ | ] Waiting for .DLL...");
+                string dllPath = Program.OpenFileBrowser("Select the DLL file to inject", "DLL Files (*.dll)|*.dll");
+                if (string.IsNullOrEmpty(dllPath))
+                {
+                    Console.Clear();
+                    Console.WriteLine("No DLL selected. Exiting.");
+                    Pause();
+                    return;
+                }
+                int processId = Program.getProcessId(processName);
+                if (processId != -1)
+                {
+                    Injector.InjectDll(processId, dllPath);
+                    Console.Clear();
+                    Console.WriteLine("DLL injection successful. :D");
+                    Console.WriteLine("https://regularlunar.pages.dev");
+                    Pause();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Process not found. Open the process before injecting. ;)");
+                    Pause();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine("Error: " + ex.Message);
+                Pause();
+            }
+        }
+
+        private static string OpenFileBrowser(string title, string filter)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Title = title;
+                ofd.Filter = filter;
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    return ofd.FileName;
+                }
+            }
+            return null;
+        }
+
+        private static int getProcessId(string processName)
+        {
+            int result;
+            try
+            {
+                Process[] processes = Process.GetProcessesByName(processName);
+                if (processes.Length != 0)
+                {
+                    result = processes[0].Id;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("No process with the specified name was found.");
+                    Pause();
+                    result = -1;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine("Error retrieving process ID: " + ex.Message);
+                Pause();
+                result = -1;
+            }
+
+            return result;
+        }
+        private static void Pause()
+        {
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
